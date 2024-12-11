ed457d358a7285eefc9577677e08b4c684a64942
Nate Z <ntzeitlin@gmail.com>
2024-12-11 11:57:42 -0600

    added transient state tracker

--- scripts/OwnsJeans.js
+++ scripts/OwnsJeans.js
@@ -1,6 +1,17 @@
 // component function
 
+import { setOwnsBlueJeans } from "./TransientState.js"
+
+const handleOwnershipChange = (changeEvent) => {
+    if (changeEvent.target.name === "ownsJeans") {
+        const convertedToBoolean = JSON.parse(changeEvent.target.value)
+        setOwnsBlueJeans(convertedToBoolean)
+    }
+}
+
 export const OwnsJeansChoices = () => {
+    document.addEventListener("change", handleOwnershipChange)
+
     let choicesHTML = "<h2>Do you own a pair of jeans?</h2>"
     choicesHTML += "<input type='radio' name='ownsJeans' value='true' /> Yes"
     choicesHTML += "<input type='radio' name='ownsJeans' value='false' /> No"
--- scripts/UrbanDweller.js
+++ scripts/UrbanDweller.js
@@ -1,6 +1,18 @@
+import { setSocioLocationId } from "./TransientState.js"
+
+const handleLocationChange = (changeEvent) => {
+    if (changeEvent.target.name === 'location') {
+        const convertedToInt = parseInt(changeEvent.target.value)
+        setSocioLocationId(convertedToInt)
+    }
+}
+
+
 export const LocationTypeChoices = async () => {
     const response = await fetch("http://localhost:8088/socioLocations")
     const locations = await response.json()
+
+    document.addEventListener("change", handleLocationChange)
 
     let choicesHTML = "<h2>Which type of area do you live in?</h2>"
     for (const location of locations) {
